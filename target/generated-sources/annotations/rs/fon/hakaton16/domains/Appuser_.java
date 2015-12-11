a3ef3717213f35b0ee3cfe0dd1600cf3f3051e82
ssstefan <stefan93ss@hotmail.com>
2015-12-11 02:09:24 +0100

    tc 8.0.27

--- target/generated-sources/annotations/rs/fon/hakaton16/domains/Appuser_.java
+++ target/generated-sources/annotations/rs/fon/hakaton16/domains/Appuser_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-11T01:57:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-12-11T02:08:28")
 @StaticMetamodel(Appuser.class)
 public class Appuser_ { 
 
